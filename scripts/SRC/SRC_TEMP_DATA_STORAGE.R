885ced42b1c7a6a20ebf3e4d5bc8354efc0d07ab
Laurigit <irualgm@gmail.com>
2019-07-10 15:41:13 +0000

    tehdään temp_data_storage jos ei oo.

--- scripts/SRC/SRC_TEMP_DATA_STORAGE.R
+++ scripts/SRC/SRC_TEMP_DATA_STORAGE.R
@@ -2,5 +2,24 @@
 
 
 required_functions("rc")
+if (file.exists("../common_data/")) {
 readtds <- rc("temp_data_storage.csv")
+} else {
+  muuttujat<-c(
+    "Aloitus_DT",
+    "Laurin_mulligan",
+    "Martin_mulligan",
+    "Peli_ID")
+  arvot <- c(
+    as.character(now()),
+    0,
+    0,
+    1)
+  tempData <- data.table(muuttuja = muuttujat, arvo = arvot)
+
+
+
+
+  wc(tempData, "../common_data/", "temp_data_storage" )
+}
 SRC_TEMP_DATA_STORAGE <- readtds
