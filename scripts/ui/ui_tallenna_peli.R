a41aa607bba08df7ff8a605260e5131bd5e3a3ab
ll <l@l.com>
2023-08-06 10:08:17 +0300

    changed maximum cards at the end of game to 10

--- scripts/ui/ui_tallenna_peli.R
+++ scripts/ui/ui_tallenna_peli.R
@@ -6,20 +6,20 @@ tabItem(tabName="tab_tallenna_peli",
                                         value = 1),
                           actionButton("laurin_virhe","Laurin virhe")),
                    column(2,      radioButtons("radio_voittaja", label = h3("Voittaja"),
-                                               choices = list("Lauri voitti" = 0, "Martti voitti" = 1), 
-                                               selected = 1)  
-                          
+                                               choices = list("Lauri voitti" = 0, "Martti voitti" = 1),
+                                               selected = 1)
+
                    ),
                    column(2,textOutput("last_changed_value_text"),
-                          
+
                           tags$head(tags$style("#last_changed_value_text{color: green;
                                                font-size: 100px;
                                                font-style: bold;
                                                }"
                               )
                           )
-                          
-                          
+
+
                           ),
                    column(4,
                           sliderInput("slider_martin_virhe",
@@ -34,7 +34,7 @@ tabItem(tabName="tab_tallenna_peli",
                           sliderInput("slider_laurin_landit",
                                       label = h4("Lauri landit"),
                                       min = 0,
-                                      max = 10, 
+                                      max = 10,
                                       value = 0)),
                    #column(4,sliderInput("slider_vuoroarvio",label=h4("Vuoroarvaus"),min=0,max=10,value=0)),
                    column(4, uiOutput("vuoroArvausPelaaja")),
@@ -47,26 +47,26 @@ tabItem(tabName="tab_tallenna_peli",
           fluidRow(column(6,
                           sliderInput("slider_laurin_kasikortit",
                                       label = h4("Laurin kasikortit"),
-                                      min=-1,max=7,value=-1)),
-                   
-                   column(6, sliderInput("slider_martin_kasikorit",label=h4("Martin kasikortit"),min=-1,max=7,value=-1))),
+                                      min = -1, max = 8,value=-1)),
+
+                   column(6, sliderInput("slider_martin_kasikorit",label=h4("Martin kasikortit"),min=-1,max=8,value=-1))),
           fluidRow(column(6,sliderInput("slider_laurin_lifet",label=h4("Laurin lifet"),min=0,max=21,value=0)),
-                   
+
                    column(6, sliderInput("slider_martin_lifet",label=h4("Martin lifet"),min=0,max=21,value=0))),
-          
+
           fluidRow(column(3,actionButton("tallenna_tulos","Tallenna tulos")),
                    column(3, textOutput("validateWinnerText"),
-                          
+
                           tags$head(tags$style("#validateWinnerText{color: red;
                                  font-size: 20px;
                                                    font-style: bold;
                                                    }"
                           )
                           )
-                          
+
                    ),
                    column(3,actionButton("action_reduce",label="",icon("arrow-left"),width='100%')),
                    column(3,actionButton("action_add",label="",icon("arrow-right"),width='100%')))
-          
+
           )
         )
