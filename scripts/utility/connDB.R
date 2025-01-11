38c48b17cc48a73fd5c0f9409f62020dbaba5cff
ll <l@l.com>
2025-01-11 15:27:08 +0200

    updated new gcp ip address

--- scripts/utility/connDB.R
+++ scripts/utility/connDB.R
@@ -1,15 +1,15 @@
 connDB <- function(con) {
   con <- tryCatch({
-    
+
     res <- dbFetch(dbSendQuery(con, "SHOW TABLES"))
-    
+
     con
   }, error = function(ef) {
-    
+
     con <<- dbConnect(MySQL(),
                       user = 'root',
                       password = 'betmtg_pw',
-                      host = '35.228.73.82',
+                      host = '34.88.252.73',
                       port = 3306,
                       dbname = 'betmtg2')
   })
